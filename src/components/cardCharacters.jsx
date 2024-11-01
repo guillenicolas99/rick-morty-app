49f050c0bfcba052f20bc2c3f8f608ccd97b2ed4
Guillermo <guillenicolas99@gmail.com>
2024-11-01 08:45:47 -0600

    commit

--- src/components/cardCharacters.jsx
+++ src/components/cardCharacters.jsx
@@ -10,7 +10,7 @@ export default function Cardharacters({ name, species, status, image, children, 
     return (
         <article className='bg-slate-900 text-white rounded-md overflow-hidden hover:scale-105 transition-all'>
 
-            <img src={image} alt={name} className="w-full h-80 object-cover" />
+            <img src={image} alt={name} className="object-cover" />
 
             <div className="p-3">
                 <h2>{name}</h2>
